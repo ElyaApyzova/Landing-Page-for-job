010a66cbeac682444b0bdcc7852fe2593aee43a2
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-06-18 22:22:21 +0300

    data

---
+++ JS/data.js
@@ -0,0 +1,38 @@
+const data = [
+    {
+        image: "images/participant's img.png",
+        title: "Хозе-Рауль Капабланка",
+        description: "Чемпион мира по шахматам",
+        btn: "$Подробнее",
+    },
+    {
+        image: "images/participant's img.png",
+        title: "Эммануил Ласкер",
+        description: "Чемпион мира по шахматам",
+        btn: "Подробнее",
+    },
+    {
+        image: "images/participant's img.png",
+        title: "Александр Алехин",
+        description: "Чемпион мира по шахматам",
+        btn: "Подробнее",
+    },
+    {
+        image: "images/participant's img.png",
+        title: "Арон Нимцович",
+        description: "Чемпион мира по шахматам",
+        btn: "Подробнее",
+    },
+    {
+        image: "images/participant's img.png",
+        title: "Рихард Рети",
+        description: "Чемпион мира по шахматам",
+        btn: "Подробнее",
+    },
+    {
+        image: "images/participant's img.png",
+        title: "Остап Бендер",
+        description: "Гроссмейстер",
+        btn: "Подробнее",
+    },
+]
